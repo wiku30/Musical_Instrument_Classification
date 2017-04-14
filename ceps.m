4c6df4680114238b8126c9cb71ccc214b0269d75
wiku30 <wingedkuriboh@126.com>
2017-04-14 10:09:36 +0800

    ceps model added

---
+++ ceps.m
@@ -0,0 +1,26 @@
+function varargout=ceps(varargin)
+    filename=varargin{1};
+
+    [in1,fs]=audioread(filename);
+    n=round(fs*0.1);
+    if(nargin<=1)
+        range=1:n;
+    else
+        range = round(fs*varargin{2}+1) : round(fs*varargin{2}+n) ; 
+    end
+    sig=in1(range,1);
+    for i=1:n
+        sig(i)=sig(i)*power(sin(i/n*pi),2); %±ÜÃâ±ßÔµÃ«´Ì
+    end
+    freq=fft(sig,n);
+    logamp=log(abs(freq));
+    cp=abs(ifft(logamp));
+    subplot(2,1,1);
+    plot(0:1/n:1-1/n,cp(1:n));
+    varargout{1}=cp;
+    xlim([0.002,0.1]); % base: 100-5000Hz
+    ylim([0,0.7]);
+    if(nargout>=2)
+        varargout{2}(1)=mean(varargout{1}(round(0.005*n):round(0.1*n)));
+    end
+end
--- main.m
+++ main.m
@@ -1,2 +1,4 @@
-
-P2=spec('wav/in5.wav');
+figure(1);
+[L1,m1]=ceps('wav/music/ÃÎÏç.wav',79.3);
+figure(2);
+spec('wav/music/ÃÎÏç.wav',79.3);
