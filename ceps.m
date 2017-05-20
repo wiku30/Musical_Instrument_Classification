6ba74d79d84e561ccaf28298f50012ee9a2cf7c3
wiku30 <wingedkuriboh@126.com>
2017-05-20 14:21:30 +0800

    improvement of ver 5-20

--- ceps.m
+++ ceps.m
@@ -27,20 +27,34 @@ function varargout=ceps(varargin)
         varargout{2}(1)=mean(varargout{1}(round(0.005*n):round(0.1*n)));
     end
 
-    absmax=0;
+    absmax=0; 
     argmax=0;
     k=0;
-    maxth=0.2;
-    flatth=0.05;
-    width=25;
+    maxth=0.2; % min ratio between consequent peaks
+    flatth=0.03;
+    width=12;
 
     for i=width+1:n-width
+        if(i>n-width)
+            break;
+        end
         if (cp(i) == max(cp(i-width:i+width)) && cp(i)>absmax*maxth && cp(i)>flatth)
+            if(k==0)
+                width = round(0.7*i); 
+                if(cp(i)~=max(cp(i-width:i+width)))
+                    continue;
+                end
+                % For fundamental freq,
+                % Assume no chord at the moment
+                % will be improved afterwards 
+                % (By only considering the basic series)
+            end
             argmax=i;
             absmax=cp(i);
             k=k+1;
             peak(k,1)=(i-1)/xmax;
             peak(k,2)=absmax;
+
             text(peak(k,1)/fs,peak(k,2),num2str(round(peak(k,2)*100)));
         end
     end
