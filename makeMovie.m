8c53eb7422c78a2649e65fd7cfd8c89406417ad2
wiku30 <wingedkuriboh@126.com>
2017-06-16 14:50:36 +0800

    background

--- makeMovie.m
+++ makeMovie.m
@@ -17,18 +17,29 @@ figure %新建一张图
 axis off
 axis([0 5 -1 6]);%定义x轴（从0到5）和y轴的范围（从0到2）
 set(gcf, 'position', [0 0 1000 1000]);
-ins(1)=text(0.2,0,'Guitar','fontsize',60,'color','black');
-ins(2)=text(0.2,1,'Sax','fontsize',60,'color','black');
-ins(3)=text(0.2,2,'Flute','fontsize',60,'color','black');
-ins(4)=text(0.2,3,'Piano','fontsize',60,'color','black');
-ins(5)=text(0.2,4,'Trumpet','fontsize',60,'color','black');
-ins(6)=text(0.2,5,'Violin','fontsize',60,'color','black');
-t(1)=text(0.5,1,'Start','fontsize',80,'color','black');
-t(2)=text(0.5,1,'Start','fontsize',80,'color','black');
-t(3)=text(0.5,1,'Start','fontsize',80,'color','black');
-t(4)=text(0.5,1,'Start','fontsize',80,'color','black');
-t(5)=text(0.5,1,'Start','fontsize',80,'color','black');
-t(6)=text(0.5,1,'Start','fontsize',80,'color','black');
+
+ha=axes('units','normalized','position',[0 0 1 1]);
+uistack(ha,'down')
+II=imread('photo/Star04.jpg');
+image(II)
+colormap gray
+set(ha,'handlevisibility','off','visible','off');
+
+set(gca,'color','none') %这里以前自己没有注意
+
+
+ins(1)=text(0.2,0,'Guitar','fontsize',60,'color','white');
+ins(2)=text(0.2,1,'Sax','fontsize',60,'color','white');
+ins(3)=text(0.2,2,'Flute','fontsize',60,'color','white');
+ins(4)=text(0.2,3,'Piano','fontsize',60,'color','white');
+ins(5)=text(0.2,4,'Trumpet','fontsize',60,'color','white');
+ins(6)=text(0.2,5,'Violin','fontsize',60,'color','white');
+t(1)=text(0.5,1,'Start','fontsize',80,'color','white');
+t(2)=text(0.5,1,'Start','fontsize',80,'color','white');
+t(3)=text(0.5,1,'Start','fontsize',80,'color','white');
+t(4)=text(0.5,1,'Start','fontsize',80,'color','white');
+t(5)=text(0.5,1,'Start','fontsize',80,'color','white');
+t(6)=text(0.5,1,'Start','fontsize',80,'color','white');
 prediction=zeros(1,l);
 
 
@@ -49,7 +60,7 @@ for i=1:l
         for k=1:num(j)
             n(1,k)='+';
         end
-        t(j)=text(2.5,j-1,n,'fontsize',60,'color','black');
+        t(j)=text(2.5,j-1,n,'fontsize',60,'color','white');
     end
     pause(0.1-toc);
 end
