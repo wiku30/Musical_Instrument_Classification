bcc5f3700525107a751f6ee65ff0542088fb9bc2
wanghy012 <whymahina@163.com>
2017-06-16 14:22:03 +0800

    lala

--- makeMovie.m
+++ makeMovie.m
@@ -15,36 +15,47 @@ sound(xx,44100);
 
 figure %新建一张图
 axis off
-axis([0 5 0 2])%定义x轴（从0到5）和y轴的范围（从0到2）
-t=text(0.5,1,'Start','fontsize',80,'color','black');
+axis([0 5 -1 6]);%定义x轴（从0到5）和y轴的范围（从0到2）
+set(gcf, 'position', [0 0 1000 1000]);
+ins(1)=text(0.2,0,'Guitar','fontsize',60,'color','black');
+ins(2)=text(0.2,1,'Sax','fontsize',60,'color','black');
+ins(3)=text(0.2,2,'Flute','fontsize',60,'color','black');
+ins(4)=text(0.2,3,'Piano','fontsize',60,'color','black');
+ins(5)=text(0.2,4,'Trumpet','fontsize',60,'color','black');
+ins(6)=text(0.2,5,'Violin','fontsize',60,'color','black');
+t(1)=text(0.5,1,'Start','fontsize',80,'color','black');
+t(2)=text(0.5,1,'Start','fontsize',80,'color','black');
+t(3)=text(0.5,1,'Start','fontsize',80,'color','black');
+t(4)=text(0.5,1,'Start','fontsize',80,'color','black');
+t(5)=text(0.5,1,'Start','fontsize',80,'color','black');
+t(6)=text(0.5,1,'Start','fontsize',80,'color','black');
+prediction=zeros(1,l);
+
 
 for i=1:l
     tic;
     vec=anal(xx,fs,i*0.1);
     pred=predict(Mdl,vec);
+    prediction(1,i)=pred;
+    num=zeros(1,6);
+    for j=0:9
+        if (i-j)>0
+            num(prediction(i-j)+1)=num(prediction(i-j)+1)+1;
+        end
+    end
     delete(t);
-    if pred==5
-        t=text(0.5,1,'Violin','fontsize',80,'color','black');%i=1时，写一个‘百’字
-    end
-    if pred==0
-        t=text(0.5,1,'Guitar','fontsize',80,'color','black');%i=1时，写一个‘百’字
-    end
-    if pred==2
-        t=text(0.5,1,'Flute','fontsize',80,'color','black');%i=1时，写一个‘百’字
-    end
-    if pred==1
-        t=text(0.5,1,'Saxophone','fontsize',80,'color','black');%i=1时，写一个‘百’字
-    end
-    if pred==3
-        t=text(0.5,1,'Piano','fontsize',80,'color','black');%i=1时，写一个‘百’字
-    end
-    if pred==4
-        t=text(0.5,1,'Trumpet','fontsize',80,'color','black');%i=1时，写一个‘百’字
+    for j=1:6
+        n=' ';
+        for k=1:num(j)
+            n(1,k)='+';
+        end
+        t(j)=text(2.5,j-1,n,'fontsize',60,'color','black');
     end
     pause(0.1-toc);
 end
 
 delete(t);
+delete(ins)
 end
 
 
--- test1.m
+++ test1.m
@@ -1,16 +1,7 @@
 clear;
 load('Model.mat');
-<<<<<<< HEAD
+
 [xx,fs]=audioread('../canon1.mp3');
-xx=xx(:,1);
-xx=resample(xx,44100,fs);
-fs=44100;
-l=round(length(xx)/4410)-10;
-vec=zeros(l,32);
-for i=1:l
-    vec(i,:)=anal(xx,fs,i*0.1);
-=======
-[xx,fs]=audioread('pre_music/kiminona.mp3');
 xx=xx(:,1);
 xx=resample(xx,44100,fs);
 fs=44100;
@@ -23,15 +14,13 @@ vec=zeros(length,32);
 np=('');
 for i=1:length
     vec(i,:)=anal(xx,fs,i*0.2);
->>>>>>> e2257d6c844ecd2fe07018833c3b033061cdac0b
 end
 pred=predict(Mdl,vec);
 name=['Gui';'Sax';'Flu';'Pia';'Tru';'Vio'];
-for i=1:l
+for i=1:length
     np(i)=name(round(pred(i))+1);
 end
-sound(xx,44100);
-makeMovie(np);
+np
 
     
     
